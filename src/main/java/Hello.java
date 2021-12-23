4dee74f48d7a012969eb90a46eecabbc6b7493f2
mehmetcUyanik <mehmetc.uyanik@gmail.com>
2021-12-23 05:27:43 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("developing hello feature");
+        System.out.println("Local repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
